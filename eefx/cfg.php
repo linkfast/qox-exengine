26bce5a708463e67f3b3f36e22efc7c1534f5e9f
Giancarlo Chiappe Aguilar <gchiappe@qox-corp.com>
2013-10-11 19:31:54 -0500

    Version 7.0.8 Rev. 26 	Updated cfg.php.

--- eefx/cfg.php
+++ eefx/cfg.php
@@ -2,7 +2,7 @@
 /**
 @file cfg.php
 @author Giancarlo Chiappe <gch@linkfastsa.com> <gchiappe@gmail.com>
-@version 0.0.0.4
+@version 0.0.0.5
 
 @section LICENSE
 
@@ -23,68 +23,69 @@ Should be edited to match your application/server configuration and paths.
 */
 
 #Default config array :
-$ee_config = array(
-					   "http_path" => "/myapp/exengine/", # Full path to ExEngine (http://myhost:port/myapp/exengine/) (you can use /myapp/exengine/ also)
-					   "https_path" => "same", # Full path to ExEngine (SSL) (https://myhost:port/myapp/exengine/) (you can use /myapp/exengine/ also) (Set to "same" to use the http_path value)					   
-					   "pear_path" => "auto", # Set to "auto" for not changing the default include directory.				   
-					   
-					   "php_timezone" => 'America/Lima', # Supported timezones: http://www.php.net/manual/en/timezones.php
-					   
-					   #ExEngine Commander Password (Not Implemented)
-					   "eeCPassw" => "prueba1234",
-					   
-					   #ForwardMode (enables ExEngine 6 compatibility, requires ExEngine 6 also installed) (HYBRID APPS ARE NOT RECOMMENDED)
-					   "forwardmode" => false,
-					   
-					   #Debug mode (start default debugger at http://apphost/exengine_lib_path/eefx/common/debug.php or create a remote debugger):
-					   "debug" => true,
+$ee_config = 
+array(
+	"http_path" => "/myapp/exengine/", # Full path to ExEngine (http://myhost:port/myapp/exengine/) (you can use /myapp/exengine/ also)
+	"https_path" => "same", # Full path to ExEngine (SSL) (https://myhost:port/myapp/exengine/) (you can use /myapp/exengine/ also) (Set to "same" to use the http_path value)					   
+	"pear_path" => "auto", # Set to "auto" for not changing the default include directory.				   
 
-					   #DevGuard (see examples)
-					   "devguard" => false, #DevGuard enable/disable (enables the lib loading, you must instanciate it for using it).
-					   "devguard_keys_path" => "", #DevGuard keys files path, full path is required (ex. /var/mykeysfolder/).
-					   
-					   #Debug/Monitor mode | Intensive database usage mode. (Not implemented)
-					   "monitor-mode"=>false, #This Enabled all traffic debugging (read documentation for more info.).
-					   "monitor-prefix"=>"ee7mon_", #Prefix for database tables in Monitor Mode.
-					   "monitor-db"=>"default", #Can be a database configuration array, or "default" to use def. database.
-					   
-					   # GetImagesFromDir Supported Images Array (in mime format)
-						"GIFD_ValidImages" => array("image/jpeg", "image/gif", "image/png")
-					   );
+	"php_timezone" => 'America/Lima', # Supported timezones: http://www.php.net/manual/en/timezones.php
+
+	#ExEngine Commander Password (Not Implemented)
+	"eeCPassw" => "prueba1234",
+
+	#ForwardMode (enables ExEngine 6 compatibility, requires ExEngine 6 also installed) (HYBRID APPS ARE NOT RECOMMENDED)
+	"forwardmode" => false,
+
+	#Debug mode (start default debugger at http://apphost/exengine_lib_path/eefx/common/debug.php or create a remote debugger):
+	"debug" => true,
+
+	#DevGuard (see examples)
+	"devguard" => false, #DevGuard enable/disable (enables the lib loading, you must instanciate it for using it).
+	"devguard_keys_path" => "", #DevGuard keys files path, full path is required (ex. /var/mykeysfolder/).
+
+	#Debug/Monitor mode | Intensive database usage mode. (Not implemented)
+	"monitor-mode"=>false, #This Enabled all traffic debugging (read documentation for more info.).
+	"monitor-prefix"=>"ee7mon_", #Prefix for database tables in Monitor Mode.
+	"monitor-db"=>"default", #Can be a database configuration array, or "default" to use def. database.
+
+	# GetImagesFromDir Supported Images Array (in mime format)
+	"GIFD_ValidImages" => array("image/jpeg", "image/gif", "image/png")
+);
 
 #Default database array :
-$ee_ddb = array(
-					"type" => "mysqli", 
-					#     TYPE     |   DESCRIPTION  |    SUPPORT     |                NOTES                 #
-					###############|#########################################################################
-					# mysql        | MySQL DB.      | PARTIAL/NATIVE | Deprecated, support up to PHP 5.4.9  #
-					###############|#########################################################################
-					# mysqli       | MySQL DB.      | PARTIAL/NATIVE | Recommended MySQL Driver             #
-					#              |                |                | (requires mysqli extension)          #
-					###############|#########################################################################
-					# pgsql        | PostgreSQL.    | NOT YET/NATIVE |                                      #
-					#########################################################################################
-					# sqlite       | SQLite (file). | NOT YET/NATIVE | host field specifies the path to db. #
-					###############|#########################################################################
-					# *edbl_driver | For non-native | PARTIAL/NON-   | EDBL Drivers are stored in           #
-					#              | databases.     | NATIVE         | lib/edbl/ name should be provided as #
-					#              |                |                | the first part of the filename. (for #
-					#              |                |                | db2.edbl.php just write db2 as type) #
-					###############|#########################################################################
-					# pdo          | For non-native | NOT YET/NON-   | Do not use PDO if there is a NATIVE  #
-					#              | PDO database   | NATIVE         | or EDBL driver available.            #
-					#              | drivers.       |                |                                      #
-					###############|#########################################################################
-					#"pdo_url" => "", 
-					# NOTE: If you use type => "pdo" the following array keys will be omitted (host,user,passwd,etc) and you must write the pdo url
-					# in the "pdo_url" value.
-					"host" => "localhost", 
-					#NOTE: if type=>"sqlite", set "host" => "path/to/file/file.db" (relative to the exengine core php)
-					"user" => "mydbuser", #only mysql/i and pgsql
-					"passwd" => "mydbpw", #only mysql/i and pgsql
-					"db" => "mydb", #only mysql/i and pgsql
-					# "port" => "", #only mysqli and pgsql
-					"utf8mode" => true #Enables UTF8 Compatibility Mode
-				);
-
+$ee_ddb = 
+array(
+	"type" => "mysqli", 
+	#     TYPE     |   DESCRIPTION  |    SUPPORT     |                NOTES                 #
+	###############|#########################################################################
+	# mysql        | MySQL DB.      | PARTIAL/NATIVE | Deprecated, support up to PHP 5.4.9  #
+	###############|#########################################################################
+	# mysqli       | MySQL DB.      | PARTIAL/NATIVE | Recommended MySQL Driver             #
+	#              |                |                | (requires mysqli extension)          #
+	###############|#########################################################################
+	# pgsql        | PostgreSQL.    | NOT YET/NATIVE |                                      #
+	#########################################################################################
+	# sqlite       | SQLite (file). | NOT YET/NATIVE | host field specifies the path to db. #
+	###############|#########################################################################
+	# *edbl_driver | For non-native | PARTIAL/NON-   | EDBL Drivers are stored in           #
+	#              | databases.     | NATIVE         | lib/edbl/ name should be provided as #
+	#              |                |                | the first part of the filename. (for #
+	#              |                |                | db2.edbl.php just write db2 as type) #
+	###############|#########################################################################
+	# pdo          | For non-native | NOT YET/NON-   | Do not use PDO if there is a NATIVE  #
+	#              | PDO database   | NATIVE         | or EDBL driver available.            #
+	#              | drivers.       |                |                                      #
+	###############|#########################################################################
+	#"pdo_url" => "", 
+	# NOTE: If you use type => "pdo" the following array keys will be omitted (host,user,passwd,etc) and you must write the pdo url
+	# in the "pdo_url" value.
+	"host" => "localhost", 
+	#NOTE: if type=>"sqlite", set "host" => "path/to/file/file.db" (relative to the exengine core php)
+	"user" => "mydbuser", #only mysql/i and pgsql
+	"passwd" => "mydbpw", #only mysql/i and pgsql
+	"db" => "mydb", #only mysql/i and pgsql
+	# "port" => "", #only mysqli and pgsql
+	"utf8mode" => true #Enables UTF8 Compatibility Mode				
+);
 ?>
